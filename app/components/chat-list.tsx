553e7aebbb816805c8df8a85ed97a7fcb6fcd92e
Oliver Klingefjord <oliverklingefjord@gmail.com>
2023-07-26 16:38:34 +0200

    card react component

--- app/components/chat-list.tsx
+++ app/components/chat-list.tsx
@@ -2,20 +2,28 @@ import { type Message } from "ai"
 
 import { Separator } from "./ui/separator"
 import { ChatMessage } from "./chat-message"
+import { ValuesCard as ValuesCardType } from "~/lib/consts"
+import ValuesCard from "./ui/values-card"
 
 export interface ChatList {
   messages: Message[]
+  valueCards: { position: number; card: ValuesCardType }[]
 }
 
-export function ChatList({ messages }: ChatList) {
+export function ChatList({ messages, valueCards }: ChatList) {
   if (!messages.length) {
     return null
+  }
+
+  const valueCard = (index: number) => {
+    return valueCards.find((card) => card.position === index)
   }
 
   return (
     <div className="relative mx-auto max-w-2xl px-4">
       {messages.map((message, index) => (
         <div key={index}>
+          {valueCard(index) && <ValuesCard card={valueCard(index)!.card} />}
           <ChatMessage message={message} />
           {index < messages.length - 1 && (
             <Separator className="my-4 md:my-8" />
--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -13,19 +13,11 @@ export interface ChatProps extends React.ComponentProps<"div"> {
   id?: string
 }
 
-function ValuesCard({ card }: { card: ValuesCardType }) {
-  return (
-    <div className="mt-4 border border-2 border-stone-300 rounded-xl p-8 max-w-[420px]">
-      <p className="text-md font-bold">{card.title}</p>
-      <p className="text-md text-neutral-500">{card.instructions_short}</p>
-      <p className="text-sm font-bold pt-2 font-bold text-stone-300">HOW?</p>
-      <p className="text-sm text-neutral-500">{card.instructions_detailed}</p>
-    </div>
-  )
-}
-
 export function Chat({ id, initialMessages, className }: ChatProps) {
-  const [valuesCard, setValuesCard] = useState<ValuesCardType | null>(null)
+  const [valueCards, setValueCards] = useState<
+    { position: number; card: ValuesCardType }[]
+  >([])
+
   const { messages, append, reload, stop, isLoading, input, setInput } =
     useChat({
       id,
@@ -36,9 +28,16 @@ export function Chat({ id, initialMessages, className }: ChatProps) {
       initialMessages,
       onResponse: async (response) => {
         if (response.headers.has("X-Values-Card")) {
-          setValuesCard(
-            JSON.parse(response.headers.get("X-Values-Card")!) as ValuesCardType
-          )
+          setValueCards((prev) => [
+            ...prev,
+            {
+              // The last user & assistant pair has not been appended yet.
+              position: messages.length + 1,
+              card: JSON.parse(
+                response.headers.get("X-Values-Card")!
+              ) as ValuesCardType,
+            },
+          ])
         }
 
         if (response.status === 401) {
@@ -46,12 +45,13 @@ export function Chat({ id, initialMessages, className }: ChatProps) {
         }
       },
     })
+
   return (
     <>
       <div className={cn("pb-[200px] pt-4 md:pt-10", className)}>
         {messages.length ? (
           <>
-            <ChatList messages={messages} />
+            <ChatList messages={messages} valueCards={valueCards} />
             <ChatScrollAnchor trackVisibility={isLoading} />
           </>
         ) : (
