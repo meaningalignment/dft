02d20a11fde7c921b7cb400c373be9644fa83768
Oliver Klingefjord <oliverklingefjord@gmail.com>
2023-08-01 15:03:47 +0200

    small ui fix

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <div className="flex-grow" />
-      <div className="flex items-center justify-end space-x-2">
+      <div className="flex items-center justify-end">
         <p className="text-sm text-gray-400">{user?.email}</p>
         <form action="/auth/logout" method="post">
           <Button variant={"link"}>Sign Out</Button>
