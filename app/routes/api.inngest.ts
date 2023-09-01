3f4b4d00fafac09abd95c6c1fba05bb25bee59da
Oliver Klingefjord <oliverklingefjord@gmail.com>
2023-09-01 17:37:25 +0200

    added temoporary function for debugging

--- app/routes/api.inngest.ts
+++ app/routes/api.inngest.ts
@@ -1,9 +1,14 @@
 import { serve } from "inngest/remix"
-import { deduplicate } from "~/services/deduplication"
+import { deduplicate, deduplicateManual } from "~/services/deduplication"
 import { embed } from "~/services/embedding"
 import { inngest } from "~/config.server"
 import { hypothesize } from "~/services/linking-routing"
 
-const handler = serve(inngest, [deduplicate, embed, hypothesize])
+const handler = serve(inngest, [
+  deduplicate,
+  embed,
+  deduplicateManual,
+  hypothesize,
+])
 
 export { handler as loader, handler as action }
