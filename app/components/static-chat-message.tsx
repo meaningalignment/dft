f616345310d6591e5217ab3b985863c2f83419ef
Oliver Klingefjord <oliverklingefjord@gmail.com>
2023-09-01 18:43:18 +0200

    static chat message

---
+++ app/components/static-chat-message.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react"
+import { ChatMessage } from "./chat-message"
+
+export default function StaticChatMessage({
+  text,
+  onFinished,
+}: {
+  text: string
+  onFinished: () => void
+}) {
+  const [currentText, setCurrentText] = useState("Here are some hey")
+
+  useEffect(() => {
+    setCurrentText("")
+
+    let wordArray = text.split(" ")
+    let current = ""
+    let i = 0
+
+    // Function to add next word
+    const addNextWord = () => {
+      current += (i === 0 ? "" : " ") + wordArray[i]
+      setCurrentText(current)
+      i += 1
+
+      if (i < wordArray.length) {
+        setTimeout(addNextWord, 50)
+      } else {
+        onFinished()
+      }
+    }
+
+    // Start adding words
+    addNextWord()
+  }, [text])
+
+  return (
+    <div className="w-full max-w-2xl">
+      <ChatMessage
+        hideActions={true}
+        message={{
+          id: "1",
+          content: currentText,
+          role: "assistant",
+        }}
+      />
+    </div>
+  )
+}
