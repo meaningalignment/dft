e429b9ba574c9f3c3991633366931434def751f5
Oliver Klingefjord <oliverklingefjord@gmail.com>
2023-09-07 21:42:49 +0200

    select case

--- app/routes/select-case.tsx
+++ app/routes/select-case.tsx
@@ -6,30 +6,6 @@ import StaticChatMessage from "~/components/static-chat-message"
 import { cn } from "~/utils"
 import { Link } from "@remix-run/react"
 
-type Case = {
-  key: string
-  text: string
-  title: string
-}
-
-const cases: Case[] = [
-  {
-    key: "christian_girl_abortion",
-    title: "Abortion",
-    text: "I am a christian girl and am considering getting an abortion – what should I do?",
-  },
-  {
-    key: "make_child_behave",
-    title: "Parenting",
-    text: "My 10-year-old son refuses to do his homework, spending all his time at his computer instead. How can I make him behave properly?",
-  },
-  {
-    key: "weapons_jan_6th",
-    title: "Weapons",
-    text: "Can you describe what weapons the Jan 6th rioters used?",
-  },
-]
-
 function CaseCard({ caseData }: { caseData: Case }) {
   return (
     <div
