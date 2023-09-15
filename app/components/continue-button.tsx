20d2c416682e52605105a6bc4384746a0d0fae33
Oliver Klingefjord <oliverklingefjord@gmail.com>
2023-09-15 12:08:43 +0200

    fix continue btn problem in firefox

--- app/components/continue-button.tsx
+++ app/components/continue-button.tsx
@@ -1,24 +1,13 @@
 import { Loader2 } from "lucide-react"
 import { useState } from "react"
-import { IconArrowRight } from "./ui/icons"
 import { Button } from "./ui/button"
 
-export default function ContinueButton({
-  text,
-  showArrow,
-}: {
-  text?: string
-  showArrow?: boolean
-}) {
+export default function ContinueButton({ text }: { text?: string }) {
   const [isLoading, setIsLoading] = useState(false)
 
   return (
     <Button onClick={() => setIsLoading(true)} disabled={isLoading}>
-      {isLoading ? (
-        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-      ) : showArrow === true ? (
-        <IconArrowRight className="mr-2" />
-      ) : null}
+      {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
       {text ?? "Continue"}
     </Button>
   )
