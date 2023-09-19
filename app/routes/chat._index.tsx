bc70078ce54910ea764d1c9dfcb198c97ba70899
Joe Edelman <joe.edelman@gmail.com>
2023-09-19 21:28:55 +0200

    oops

--- app/routes/chat._index.tsx
+++ app/routes/chat._index.tsx
@@ -4,7 +4,7 @@ import { articulatorConfig } from "~/cookies.server"
 
 export async function loader({ request }: LoaderArgs) {
   const url = new URL(request.url)
-  let prompt = url.searchParams.get("prompt") || "default"
+  let prompt = url.searchParams.get("prompt")
   // randomize it!
   if (!prompt) {
     prompt = Math.random() > 0.5 ? "default" : "guess_card"
