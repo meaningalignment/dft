1d207a7cc2ce3c09ca80779f93bb04117afa71b8
Joe Edelman <joe.edelman@gmail.com>
2023-09-19 21:35:57 +0200

    randomize articulator config

--- app/routes/case.$caseId.chat._index.tsx
+++ app/routes/case.$caseId.chat._index.tsx
@@ -1,7 +1,18 @@
 import { v4 as uuid } from "uuid"
 import { LoaderArgs, redirect } from "@remix-run/node"
+import { articulatorConfig } from "~/cookies.server"
 
-export async function loader({ params }: LoaderArgs) {
+export async function loader({ request, params }: LoaderArgs) {
   const caseId = params.caseId!
-  return redirect(`/case/${caseId}/chat/${uuid()}`)
+  const url = new URL(request.url)
+  let prompt = url.searchParams.get("prompt")
+  // randomize it!
+  if (!prompt) {
+    prompt = Math.random() > 0.5 ? "default" : "guess_card"
+  }
+  return redirect(`/case/${caseId}/chat/${uuid()}`, {
+    headers: {
+      "Set-Cookie": await articulatorConfig.serialize(prompt),
+    },
+  })
 }
