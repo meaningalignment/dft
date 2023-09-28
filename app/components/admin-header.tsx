f3517bf897c915657cb43045715f1146ec4ed2c3
Oliver Klingefjord <oliverklingefjord@gmail.com>
2023-09-28 15:39:31 -0700

    copy

--- app/components/admin-header.tsx
+++ app/components/admin-header.tsx
@@ -1,21 +1,12 @@
-import { User } from "@prisma/client"
-import { useCurrentUser } from "../root"
-import { Button } from "./ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "./ui/dropdown-menu"
-import { Form, Link, NavLink as RemixNavLink } from "@remix-run/react"
-import { useRef } from "react"
+import { NavLink as RemixNavLink } from "@remix-run/react"
 
 function NavLink({ children, ...props }: any) {
   return (
     <RemixNavLink
       {...props}
-      className={({ isActive }) => `${isActive ? 'text-zinc-800' : 'text-zinc-400'} hover:text-zinc-500`}
+      className={({ isActive }) =>
+        `${isActive ? "text-zinc-800" : "text-zinc-400"} hover:text-zinc-500`
+      }
     >
       {children}
     </RemixNavLink>
@@ -25,7 +16,7 @@ function NavLink({ children, ...props }: any) {
 export default function AdminHeader() {
   return (
     <header className="sticky top-0 z-50 flex items-center justify-start gap-3 w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
-      <b className="text-lg mr-2">Admin stuff</b>
+      <b className="text-lg mr-2">Admin</b>
       <NavLink to="/admin/cards">Cards</NavLink>
       <NavLink to="/admin/merge">Merge</NavLink>
       <NavLink to="/admin/chats">Chats</NavLink>
