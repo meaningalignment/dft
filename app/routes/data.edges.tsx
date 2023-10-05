bb412fc0eada082470a42b82b79042bdd8881b0e
Joe Edelman <joe.edelman@gmail.com>
2023-10-05 19:59:24 +0200

    inline buildgraph

--- app/routes/data.edges.tsx
+++ app/routes/data.edges.tsx
@@ -1,15 +1,58 @@
 
 // import { useLoaderData } from "@remix-run/react";
 import { useEffect, useState } from "react";
-// import { buildGraph } from './data.edges[.]json.js'
 import { json } from '@remix-run/node';
+import { Edge } from "@prisma/client";
+import { db } from "~/config.server";
 
 export const config = {
   maxDuration: 300
 }
 
+function score(edge: Edge) {
+  if (edge.relationship === "upgrade") return 1
+  if (edge.relationship === "no_upgrade") return -0.2
+  else return 0
+}
+
+async function buildGraph() {
+  const nodes = await db.canonicalValuesCard.findMany()
+  const referencedNodeIds = new Set<number>()
+  const edgesFromDb = await db.edge.findMany()
+  const pairs: { [k: string]: number[] } = {}
+  for (const edge of edgesFromDb) {
+    if (edge.fromId < edge.toId) {
+      const k = `${edge.fromId}-${edge.toId}`
+      const existing = pairs[k] ?? []
+      pairs[k] = [...existing, score(edge)]
+    } else {
+      const k = `${edge.toId}-${edge.fromId}`
+      const existing = pairs[k] ?? []
+      pairs[k] = [...existing, -score(edge)]
+    }
+  }
+  const links = Object.entries(pairs)
+    .map(([k, scores]) => {
+      const [fromId, toId] = k.split("-").map(Number)
+      const total = scores.reduce((a, b) => a + b, 0)
+      const avg = total / scores.length
+      const count = scores.length
+      if (avg > 0) return { source: fromId, target: toId, count, avg }
+      else return { target: fromId, source: toId, count, avg: -avg }
+    })
+    .filter((w) => w.count > 1)
+
+  for (const link of links) {
+    referencedNodeIds.add(link.source)
+    referencedNodeIds.add(link.target)
+  }
+
+  return { nodes: nodes.filter((n) => referencedNodeIds.has(n.id)), links }
+}
+
+
 export async function loader() {
-  // const { nodes, links } = await buildGraph()
+  const { nodes, links } = await buildGraph()
   return json({ nodes: [], links: [] })
 }
 
