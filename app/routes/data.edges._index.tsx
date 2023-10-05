c1dca1c3168e44082868f74d31df616f6316a131
Joe Edelman <joe.edelman@gmail.com>
2023-10-05 20:23:25 +0200

    fix moral graph

--- app/routes/data.edges._index.tsx
+++ app/routes/data.edges._index.tsx
@@ -1,10 +1,17 @@
-import Graph from "./data.edges.$caseId.js"
-import { loader as edgesLoader } from "./data.edges[.]json.js"
+import { Await, useLoaderData } from "@remix-run/react";
+import { buildGraph } from "./data.edges[.]json.js"
+import { Suspense } from "react";
+import { defer } from "@remix-run/node";
+import { MoralGraph } from "~/components/moral-graph.js";
 
 export async function loader() {
-  return await edgesLoader()
+  const graph = buildGraph()
+  return defer({ graph })
 }
 
 export default function DefaultGraphPage() {
-  return <Graph />
+  const { graph } = useLoaderData<typeof loader>();
+  return <Suspense fallback={<p>Please wait...</p>}>
+    <Await resolve={graph}>{({ nodes, links }) => <MoralGraph nodes={nodes} links={links} />}</Await>
+  </Suspense>
 }
