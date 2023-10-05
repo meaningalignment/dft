7c71f494845a0fd3313f62a3c3cb0405fca8e97f
Joe Edelman <joe.edelman@gmail.com>
2023-10-05 20:12:51 +0200

    use suspense instead of ishydraded

--- app/routes/data.edges.tsx
+++ app/routes/data.edges.tsx
@@ -1,24 +1,17 @@
 import { Await, useLoaderData } from "@remix-run/react";
-import { useEffect, useState } from "react";
 import { defer } from '@remix-run/node';
 import { Graph } from "~/components/moral-graph";
 import { buildGraph } from "./data.edges[.]json";
+import { Suspense } from "react";
 
 export async function loader() {
   const graph = buildGraph()
   return defer({ graph })
 }
 
-let isHydrating = true;
-
 export default function GraphPage() {
   const { graph } = useLoaderData<typeof loader>();
-  const [isHydrated, setIsHydrated] = useState(!isHydrating)
-  useEffect(() => {
-    isHydrating = false;
-    setIsHydrated(true);
-  }, [])
-
-  if (!isHydrated) return <p>Please wait...</p>
-  return <Await resolve={graph}>{({ nodes, links }) => <Graph nodes={nodes} links={links} />}</Await>
+  return <Suspense fallback={<p>Please wait...</p>}>
+    <Await resolve={graph}>{({ nodes, links }) => <Graph nodes={nodes} links={links} />}</Await>
+  </Suspense>
 }
