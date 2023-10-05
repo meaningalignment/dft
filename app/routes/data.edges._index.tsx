3f5a613176b412341157654c8808fe2acfcfa007
Joe Edelman <joe.edelman@gmail.com>
2023-10-05 16:04:45 +0200

    try 1

--- app/routes/data.edges._index.tsx
+++ app/routes/data.edges._index.tsx
@@ -1,6 +1,8 @@
 import Graph from "./data.edges.$caseId.js"
 import { loader as edgesLoader } from "./data.edges[.]json.js"
 
+export const config = { runtime: 'edge' }
+
 export async function loader() {
   return await edgesLoader()
 }
