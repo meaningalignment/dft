9a97de4b37f2978730a1660543f7787ed527ac12
Oliver Klingefjord <oliverklingefjord@gmail.com>
2023-10-06 22:27:02 +0200

    test 2

--- app/routes/api.test.ts
+++ app/routes/api.test.ts
@@ -1,5 +1,10 @@
 import { json } from "@remix-run/node"
 
+export const config = {
+  runtime: 'edge',
+  maxDuration: 300
+};
+
 export async function loader(): Promise<Response> {
   await new Promise((resolve) => setTimeout(resolve, 20000))
   return json({ message: "Hello world!"})
