4ee4f28d9d0145f5f6f4a4500c1b059283f8a971
Oliver Klingefjord <oliverklingefjord@gmail.com>
2023-10-08 13:50:33 +0200

    lock admin screens to admin users

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,7 +1,23 @@
 import { Outlet } from "@remix-run/react";
 import AdminHeader from "~/components/admin-header";
+import { useCurrentUser } from "~/root";
+
+function NotAuthorized() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="text-4xl font-bold mb-2">Not authorized</div>
+      <div className="text-xl">Please log in as an admin</div>
+    </div>
+  )
+}
 
 export default function Admin() {
+  const user = useCurrentUser();
+
+  if (!user || !user.isAdmin) {
+    return <NotAuthorized />
+  }
+
   return <>
     <AdminHeader />
     <Outlet />
