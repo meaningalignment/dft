17e3557a095a17cc78b6d3a42626aff72b38f944
Oliver Klingefjord <oliverklingefjord@gmail.com>
2023-10-25 12:00:40 +0200

    prolific run filter in data export

--- app/routes/api.data.edges.ts
+++ app/routes/api.data.edges.ts
@@ -1,20 +1,6 @@
 import { LoaderArgs } from "@remix-run/node"
+import { prolificRuns } from "~/lib/consts"
 import { Options, summarizeGraph } from "~/values-tools/generate-moral-graph"
-
-const prolificRuns = {
-  prolific_50: {
-    start: new Date("Sep 21, 2023 18:00:00 UTC"),
-    end: new Date("Sep 21, 2023 20:00:00 UTC"),
-  },
-  prolific_125: {
-    start: new Date("Oct 10, 2023 09:00:00 UTC"),
-    end: new Date("Oct 10, 2023 14:30:00 UTC"),
-  },
-  prolific_325: {
-    start: new Date("Oct 6, 2023 14:00:00 UTC"),
-    end: new Date("Oct 9, 2023 23:00:00 UTC"),
-  },
-}
 
 export async function loader({ request }: LoaderArgs) {
   const url = new URL(request.url)
--- app/routes/data.edges[.]json.ts
+++ app/routes/data.edges[.]json.ts
@@ -1,9 +1,35 @@
 import { LoaderArgs, json } from "@remix-run/node"
-import { summarizeGraph } from "~/values-tools/generate-moral-graph"
+import { prolificRuns } from "~/lib/consts";
+import { Options, summarizeGraph } from "~/values-tools/generate-moral-graph"
 
 export async function loader({ request }: LoaderArgs) {
   const url = new URL(request.url);
-  console.log('params', url.searchParams)
-  const graph = await summarizeGraph({ includeAllEdges: url.searchParams.get("includeAllEdges") === "true" })
+  
+  const options: Options = {}
+  
+  const runId = url.searchParams.get("runId")
+  if (runId) {
+    options.edgeWhere = options.edgeWhere || {}
+    options.edgeWhere.user = {
+      prolificId: { not: null },
+      createdAt: {
+        gte: (prolificRuns as any)[runId].start,
+        lte: (prolificRuns as any)[runId].end,
+      },
+    }
+  }
+  
+  const caseId = url.searchParams.get("caseId")
+  if (caseId) {
+    options.edgeWhere = options.edgeWhere || {}
+    options.edgeWhere.context = {
+      ContextsOnCases: { some: { caseId } },
+    }
+  }
+
+  options.includeAllEdges = url.searchParams.get("includeAllEdges") === "true"
+  
+  const graph = await summarizeGraph(options)
+  
   return json(graph)
 }
