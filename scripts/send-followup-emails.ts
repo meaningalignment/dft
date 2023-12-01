729067de52eeac232d540b4a245d8938e13703fa
Oliver Klingefjord <oliverklingefjord@gmail.com>
2023-12-01 16:52:13 +0100

    fix mg script

--- scripts/send-followup-emails.ts
+++ scripts/send-followup-emails.ts
@@ -1,15 +1,16 @@
 import { db } from "../app/config.server"
-
 import "dotenv/config"
-
-import Mailgun from "mailgun-js"
+import Mailgun from "mailgun.js"
+import formData from "form-data"
 
 ;(async () => {
   const emailTemplate = `Hello,
 
-Thank you for your valuable participation in our [Democratic Fine-Tuning](https://meaningalignment.substack.com/p/introducing-democratic-fine-tuning) experiment earlier this year. Your contributions were greatly appreciated.
+Thank you for your valuable participation in our Democratic Fine-Tuning experiment earlier this year. Your contributions were greatly appreciated.
 
-For those interested in the project's progress, we have published an [early results report on our Substack](https://meaningalignment.substack.com/p/the-first-moral-graph). The outcomes so far are very promising!
+For those interested in the project's progress, we have published an early results report on our Substack. The outcomes so far are very promising!
+
+https://meaningalignment.substack.com/p/the-first-moral-graph
 
 We have one final request. As part of our process, we deduplicated cards in the background. To validate the accuracy of this deduplication, we would like your opinion on whether the value you provided is accurately reflected in the deduplicated card.
 
@@ -17,37 +18,49 @@ Please share your thoughts here: {URL}
 
 Thank you for your continued support.
 
+
 Best,
 
-Oliver & Joe`
+Joe & Oliver`
 
-  const mg = Mailgun({
-    apiKey: process.env.MAILGUN_API_KEY,
-    domain: process.env.MAILGUN_DOMAIN,
+  const mg = new Mailgun(formData).client({
+    username: "api",
+    key: process.env.MAILGUN_API_KEY!,
+    url: process.env.MAILGUN_URL!,
   })
 
   const users = await db.user.findMany({
     where: {
-      OR: [
-        // {
-        //   email: "joeedelman@gmail.com",
-        // },
-        {
-          email: "oliverklingefjord@gmail.com",
-        },
-      ],
-    },
+      email: {
+        contains: "@"
+      },
+      chats: {
+        some: {
+          ValuesCard: {}
+        }
+      }
+    }
   })
 
+  if (users.length > 1) {
+    throw new Error("More than one user with this email")
+  }
+
   for (const user of users) {
-    await mg.messages.create(process.env.MAILGUN_DOMAIN, {
-      from: "Democratic Fine-Tuning <info@meaningalignment.org>",
-      to: user.email,
-      subject: "Democratic Fine-Tuning: Follow-up",
-      text: emailTemplate.replace(
-        "{URL}",
-        `https://dft.meaningalignment.org/data/deduplications/${user.id}`
-      ),
-    })
+    try {
+      console.log(`Sending email to ${user.email}`)
+
+      await mg.messages.create(process.env.MAILGUN_DOMAIN!, {
+        from: "Democratic Fine-Tuning <info@meaningalignment.org>",
+        to: user.email,
+        subject: "Democratic Fine-Tuning: Follow-up",
+        text: emailTemplate.replace(
+          "{URL}",
+          `https://dft.meaningalignment.org/data/deduplications/${user.id}`
+        ),
+      })
+    } catch (e) {
+      console.log(e)
+    }
   }
 })()
